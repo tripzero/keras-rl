20cff7d1c142e5cfb3fe6a5ce4e243d42a31a717
Kevron Rees <kevron.m.rees@intel.com>
2019-10-30 08:35:46 -0700

    fix atari example

--- examples/dqn_atari.py
+++ examples/dqn_atari.py
@@ -22,13 +22,16 @@ WINDOW_LENGTH = 4
 
 
 class AtariProcessor(Processor):
+
     def process_observation(self, observation):
         assert observation.ndim == 3  # (height, width, channel)
         img = Image.fromarray(observation)
-        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
+        img = img.resize(INPUT_SHAPE).convert(
+            'L')  # resize and convert to grayscale
         processed_observation = np.array(img)
         assert processed_observation.shape == INPUT_SHAPE
-        return processed_observation.astype('uint8')  # saves storage in experience memory
+        # saves storage in experience memory
+        return processed_observation.astype('uint8')
 
     def process_state_batch(self, batch):
         # We could perform this processing step in `process_observation`. In this case, however,
@@ -52,17 +55,11 @@ np.random.seed(123)
 env.seed(123)
 nb_actions = env.action_space.n
 
-# Next, we build our model. We use the same model that was described by Mnih et al. (2015).
+# Next, we build our model. We use the same model that was described by
+# Mnih et al. (2015).
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 model = Sequential()
-if K.image_dim_ordering() == 'tf':
-    # (width, height, channels)
-    model.add(Permute((2, 3, 1), input_shape=input_shape))
-elif K.image_dim_ordering() == 'th':
-    # (channels, width, height)
-    model.add(Permute((1, 2, 3), input_shape=input_shape))
-else:
-    raise RuntimeError('Unknown image_dim_ordering.')
+model.add(Permute((2, 3, 1), input_shape=input_shape))
 model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
 model.add(Activation('relu'))
 model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
@@ -85,7 +82,8 @@ processor = AtariProcessor()
 # with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
 # the agent initially explores the environment (high eps) and then gradually sticks to what it knows
 # (low eps). We also set a dedicated eps value that is used during testing. Note that we set it to 0.05
-# so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
+# so that the agent still performs some random actions. This ensures that
+# the agent cannot get stuck.
 policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
                               nb_steps=1000000)
 
@@ -102,11 +100,14 @@ dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
 if args.mode == 'train':
     # Okay, now it's time to learn something! We capture the interrupt exception so that training
-    # can be prematurely aborted. Notice that now you can use the built-in Keras callbacks!
+    # can be prematurely aborted. Notice that now you can use the built-in
+    # Keras callbacks!
     weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
-    checkpoint_weights_filename = 'dqn_' + args.env_name + '_weights_{step}.h5f'
+    checkpoint_weights_filename = 'dqn_' + \
+        args.env_name + '_weights_{step}.h5f'
     log_filename = 'dqn_{}_log.json'.format(args.env_name)
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
+    callbacks = [ModelIntervalCheckpoint(
+        checkpoint_weights_filename, interval=250000)]
     callbacks += [FileLogger(log_filename, interval=100)]
     dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
 
